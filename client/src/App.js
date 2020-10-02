ac10428d596fc8f0c57c9a46d3fa2509ab3ff758
viralthaker15 <viralthaker27@gmail.com>
2020-10-02 15:57:17 +0530

    redirecting login to homepage if token is present

--- client/src/App.js
+++ client/src/App.js
@@ -3,44 +3,43 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 /* Pages */
-import Login from './pages/login/Login';
+import Login from "./pages/login/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 /* Plugins */
-import { getToken } from './components/Helper';
-
+import { getToken } from "./components/Helper";
 
 class App extends React.Component {
-
-  state = {
-    token: false
-  }
-  componentDidMount() {
-    this.checkToken();
-  }
-  checkToken = () => {
-    let token = getToken();
-    token && this.setState({token:token});
-  }
-  render() {
-    const { token } = this.state;
-    if (!token) {
-      return <Login />
-    }
-    return (
-      <div className="main-wrap">
-
-        <Switch>
-          {/*
+	state = {
+		token: false,
+	};
+	componentDidMount() {
+		this.checkToken();
+		console.log("testing");
+	}
+	checkToken = () => {
+		let token = getToken();
+		console.log("token");
+		token && this.setState({ token: token });
+	};
+	render() {
+		const { token } = this.state;
+		if (!token) {
+			return <Login />;
+		}
+		return (
+			<div className='main-wrap'>
+				<Switch>
+					{/*
             Not secure
           */}
-          <Route exact path="/" component={Dashboard} />
-          {/* 
+					<Route exact path='/' component={Dashboard} />
+					{/* 
             Secure data 
           */}
-        </Switch>
-      </div>
-    );
-  }
+				</Switch>
+			</div>
+		);
+	}
 }
 export default App;
